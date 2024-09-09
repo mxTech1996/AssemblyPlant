dd6f63eec41291bf01a26e3c2d72463fc1a1f9e2
juliotwo <juliotwo1996@gmail.com>
2024-09-09 17:53:40 -0600

    Asss terms

--- src/data/index.js
+++ src/data/index.js
@@ -32,11 +32,11 @@ export const navData = [
 
 export const footerData = [
   {
-    href: '/pdf/AP-MESANEX-MAYO-2024.pdf',
+    href: '/pdf/AP-ASS.pdf',
     label: 'Privacy',
   },
   {
-    href: '/pdf/TYC-MESANEX-MAYO-2024.pdf',
+    href: '/pdf/TYC-ASS.pdf',
     label: 'Terms',
   },
   {
